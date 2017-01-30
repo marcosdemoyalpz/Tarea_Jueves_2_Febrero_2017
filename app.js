4f0f31688005b9634eb813c52376d70d4cdde1a3
marcosdemoyalpz <marcosdemoyalpz@gmail.com>
2017-01-30 10:37:48 -0400

    Cleaned Extra Files

--- app.js
+++ app.js
@@ -1,83 +1,65 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var bodyParser = require("body-parser");
 var app = express();
 
 app.set('json spaces', 2);
 
-err = undefined;
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.get('/favicon.ico', function(req, res) {
-    res.sendStatus(204);
-});
-
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,token");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT");
-    next();
+app.use(express.static('public'));
+
+app.patch('*', function(req, res) {
+    if (req.path.includes('/notimplemented')) {
+        res.set({
+            'Content-Type': 'text/plain',
+            'x-powered-by': 'Express',
+            'Allow': 'GET,POST,PUT'
+        })
+        res.sendStatus(501);
+    }
 });
 
-app.use(express.static('public'));
+app.put('*', function(req, res) {
+    if (req.path.includes('/notimplemented')) {
+        res.sendStatus(200);
+    }
+});
 
 app.get('*', function(req, res) {
 
     var home = true;
 
     if (req.path.includes('/protected')) {
-        err = new Error('Path is Protected');
-        err.status = 401;
+        res.sendStatus(401);
     } else if (req.path.includes('/404')) {
-        err = new Error('Not Found');
-        err.status = 404;
+        res.sendStatus(404);
     } else if (req.path.includes('/error')) {
-        err = new Error('Error');
-        err.status = 500;
+        res.sendStatus(500);
     } else if (req.path.includes('/notimplemented')) {
-        err = new Error('Not Implemented!');
-        err.status = 501;
+        res.sendStatus(200);
     } else if (req.path.includes('/login')) {
         res.sendFile(__dirname + '/public/login.html');
         home = false;
     }
 
-    if (err) {
-        console.log(err.status);
-        res.status(err.status);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    } else {
-        if (home) {
-            var myHeader, headerArray, key;
-            myHeader = JSON.parse(JSON.stringify(req.headers));
-            headerArray = [];
-            for (key in myHeader) {
-                headerArray.push(key); // Push the key on the array
-                headerArray.push(myHeader[key]); // Push the key's value on the array
-            }
-
-            var jsonObj = {
-                "method": req.method,
-                "path": req.path,
-                "host": req.host,
-                "port": req.get('host').split(':')[1],
-                "header": headerArray
-            }
-            console.log("\n");
-            console.log(jsonObj);
-            res.send(jsonObj);
+    if (home) {
+        var myHeader, headerArray, key;
+        myHeader = JSON.parse(JSON.stringify(req.headers));
+        headerArray = [];
+        for (key in myHeader) {
+            headerArray.push(key); // Push the key on the array
+            headerArray.push(myHeader[key]); // Push the key's value on the array
         }
-        // res.sendStatus(200);
+
+        var jsonObj = {
+            "method": req.method,
+            "path": req.path,
+            "host": req.hostname,
+            "port": req.get('host').split(':')[1],
+            "header": headerArray
+        }
+        res.send(jsonObj);
     }
 });
 
@@ -86,41 +68,29 @@ app.post('*', function(req, res) {
     var login = false;
 
     if (req.path.includes('/protected')) {
-        err = new Error('Path is Protected');
-        err.status = 401;
+        res.sendStatus(401);
     } else if (req.path.includes('/404')) {
-        err = new Error('Not Found');
-        err.status = 404;
+        res.sendStatus(404);
     } else if (req.path.includes('/error')) {
-        err = new Error('Error');
-        err.status = 500;
+        res.sendStatus(500);
     } else if (req.path.includes('/notimplemented')) {
-        err = new Error('Not Implemented!');
-        err.status = 501;
+        res.sendStatus(200);
     } else if (req.path.includes('/login')) {
         login = true;
     }
+    if (login) {
 
-    if (err) {
-        console.log(err.status);
-        res.status(err.status);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    } else {
-        if (login) {
+        var user = req.body.user;
+        var pass = req.body.pass;
 
-            var user = req.body.user;
-            var pass = req.body.pass;
-
-            var jsonObj = {
-                "username": user,
-                "password": pass
-            }
-            console.log(jsonObj);
-            res.send(jsonObj);
+        var jsonObj = {
+            "username": user,
+            "password": pass
         }
+        res.set({
+            'Content-Type': 'application/json'
+        })
+        res.send(jsonObj);
     }
 });
 
