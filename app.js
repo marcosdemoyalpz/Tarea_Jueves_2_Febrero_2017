2663011c97daeb5f92e5fde70daa7035cb6699d9
marcosdemoyalpz <marcosdemoyalpz@gmail.com>
2017-02-01 00:07:29 -0400

    Handlebars Views

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
 
+//NPM Module to integrate Handlerbars UI template engine with Express
+var exphbs  = require('express-handlebars');
+
+//Declaring Express to use Handlerbars template engine with main.handlebars as
+//the default layout
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Defining middleware to serve static files
+app.use('/static', express.static('public'));
+
 app.set('json spaces', 2);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.use(express.static('public'));
 
 app.patch('*', function(req, res) {
     if (req.path.includes('/notimplemented')) {
@@ -24,6 +33,10 @@ app.put('*', function(req, res) {
     if (req.path.includes('/notimplemented')) {
         res.sendStatus(200);
     }
+});
+
+app.get("/movies/create", function(req, res){
+  res.render("create")
 });
 
 app.get('*', function(req, res) {
@@ -75,7 +88,16 @@ app.post('*', function(req, res) {
         res.sendStatus(500);
     } else if (req.path.includes('/notimplemented')) {
         res.sendStatus(200);
-    } else if (req.path.includes('/login')) {
+    } else if (req.path.includes('/movies/create')) {
+        var jsonObj = {
+            "name": req.body.Name,
+            "description": req.body.Description
+        }
+        res.set({
+            'Content-Type': 'application/json'
+        })
+        res.send(jsonObj);
+    }else if (req.path.includes('/login')) {
         login = true;
     }
     if (login) {
